cdbcfa7a016dbcae09904c3794a9d0a0fbee7353
abdul munnim <munnim@localhost.localdomain>
2018-03-28 10:46:41 +0500

    uload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { SportsComponent } from './sports/sports.component';
+import { ReligionComponent } from './religion/religion.component';
+import { CookingComponent } from './cooking/cooking.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    SportsComponent,
+    ReligionComponent,
+    CookingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: "",
+        component: MainComponent,
+        children: [
+          {
+            path: "sports",
+            component: SportsComponent
+          },
+          {
+            path: "religion",
+            component: ReligionComponent
+          },
+          {
+            path:"cooking",
+            component:CookingComponent
+          }
+        ]
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
